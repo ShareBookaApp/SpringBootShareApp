f4469a92a8437f99d1bd3e29ea0b3731a1ba768f
Manny2296 <manuelfelip-96@hotmail.com>
2017-02-01 20:31:02 -0500

    First Commit

---
+++ src/main/java/com/springboot/sharebook/SpringBootShareBookApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.sharebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShareBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootShareBookApplication.class, args);
+	}
+}
