b1baac3b651ab26e63cf45272c060a7fe9723012
Manuel Felipe S.R <manuelfelip-96@hotmail.com>
2017-02-04 22:22:51 -0500

    Vista Principal diseño Responsivo

--- classes/production/SpringBootShareApp/static/app/app.js
+++ classes/production/SpringBootShareApp/static/app/app.js
@@ -3,14 +3,10 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version',
-  'myApp.view3',
-  'services.listFactory'
+
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  //$routeProvider.otherwise({redirectTo: '/index'});
 }]);
