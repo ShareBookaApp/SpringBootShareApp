001cb2738b7199c6e667a6078d72479395190741
diegomendez31 <diegomendez-4131@hotmail.com>
2017-02-22 17:48:48 -0500

    Limpieza de codigo

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'ngMaterial',
   'myApp.detalleLibro',
   'myApp.publicarLibro',
   'myApp.librosDisponibles',
