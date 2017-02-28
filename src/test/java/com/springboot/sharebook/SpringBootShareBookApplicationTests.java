2208c5453977da0b501a8b581fea54115e801d9b
Santiago Chisco <david.chisco@mail.escuelaing.edu.co>
2017-02-28 12:18:55 -0500

    Integracion Continua

--- src/test/java/com/springboot/sharebook/SpringBootShareBookApplicationTests.java
+++ src/test/java/com/springboot/sharebook/SpringBootShareBookApplicationTests.java
@@ -1,16 +1,28 @@
 package com.springboot.sharebook;
 
 import org.junit.Test;
+
+
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static junit.framework.TestCase.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootShareBookApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoadsTest() {
+		assertTrue(true);
 	}
+
+	@Test
+	public void primeraTest(){
+		assertTrue(true);
+
+    }
 
 }
