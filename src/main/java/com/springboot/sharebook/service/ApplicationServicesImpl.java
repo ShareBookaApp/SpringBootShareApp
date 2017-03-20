8a26d16499f36d81d991429ad8842d70e9dfa0d8
diegomendez31 <diegomendez-4131@hotmail.com>
2017-03-20 18:06:42 -0500

    Servicios para er implementados y Conexion Completa

---
+++ src/main/java/com/springboot/sharebook/service/ApplicationServicesImpl.java
@@ -0,0 +1,34 @@
+package com.springboot.sharebook.service;
+
+import com.springboot.sharebook.model.Libro;
+import com.springboot.sharebook.model.Solicitud;
+import com.springboot.sharebook.model.Usuario;
+import com.springboot.sharebook.repositories.LibroRepository;
+import com.springboot.sharebook.repositories.SolicitudRepository;
+import com.springboot.sharebook.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Diego on 20/03/2017.
+ */
+
+@Service
+public class ApplicationServicesImpl implements ApplicationServices {
+
+    @Autowired
+    private LibroRepository librorepo;
+    @Autowired
+    private UsuarioRepository userrepo;
+
+    @Autowired
+    private SolicitudRepository solirepo;
+
+    @Override
+    public ArrayList<Libro> traerMisLibros(String useremail) {
+        //Como este deben ser hechos los demas servicios trayendo de los repositorios
+        return null;
+    }
+}
